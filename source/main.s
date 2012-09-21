0864d327f9bf225f2b238820521f0c7180119b6b
Claudio Dettoni <dettonijr@inf.ufsc.br>
2012-09-21 14:17:36 -0300

    Added comments to OK1

--- source/main.s
+++ source/main.s
@@ -1,13 +1,25 @@
+/*
+ * GPIO controller map
+ * Base address = 0x20200000
+ * 00 - 24: Function select
+ * 28 - 36: Turn on pin
+ * 40 - 48: Turn off pin
+ * 52 - 60: Pin input
+*/
+
 .section .init
 .globl _start
 _start:
-ldr r0,=0x20200000 
-mov r1,#1
-lsl r1,#18
-str r1,[r0,#4]
+ldr r0,=0x20200000  @GPIO controller address 
+/* This block of code enable output mode on 16th pin */
+mov r1,#1           
+lsl r1,#18          @ r1 = 2<<18 #3 bits/pin, we want the 6th pin.
+str r1,[r0,#4]      @ sets the 18th bit on 0x20200004
+
+/* This block of code turns the 16th pin off */
 mov r1,#1
 lsl r1,#16
-str r1,[r0,#40]
+str r1,[r0,#40]     @sets the turn-off bit of 16th pin
 loop$:
-  b loop$
+  b loop$           @loops forever
 
