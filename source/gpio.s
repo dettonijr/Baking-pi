7551f064e8b9014ee935db1f762fdd5dfaf8f411
Claudio Dettoni <dettonijr@inf.ufsc.br>
2012-10-01 18:48:44 -0300

    OK3 OK :)

--- source/gpio.s
+++ source/gpio.s
@@ -1,4 +1,13 @@
 /*
+ * GPIO controller map
+ * Base address = 0x20200000
+ * 00 - 24: Function select
+ * 28 - 36: Turn on pin
+ * 40 - 48: Turn off pin
+ * 52 - 60: Pin input
+*/
+
+/*
  * Return gpio address on r0
  *  void* GetGpioAddress()
  */
@@ -42,4 +51,46 @@ lsl r1, r2          @r1 = r1 << r2; #Put the code at the right position
 str r1,[r0]         @store the code on GpioAddress
 pop {pc}            @return
 /* Warning: This function overwrite the previous code of the remaining pins */
-//TODO: FIX IT
+/*TODO: FIX IT*/
+
+
+/*
+ * Set GPIO pin on/off
+ *  void SetGpio(int pinNum, int pinVal)
+ * 0 <= pinNum <= 53
+ * pinVal zero or non-zero
+ */
+.globl SetGpio
+SetGpio:
+pinNum .req r0
+pinVal .req r1
+
+cmp pinNum,#53
+movhi pc,lr         @return if pinNum > 53
+
+push {lr}           @saves lr
+mov r2, pinNum      @saves pinNum on r2
+.unreq pinNum
+pinNum .req r2 
+bl GetGpioAddress    @put GpioAdd in r0
+gpioAddr .req r0
+
+pinBank .req r3
+lsr pinBank, pinNum, #5 @pinBank = pinNum/32
+lsl pinBank, #2         @compute offset of pinBank (each bank has 4 bytes)
+add gpioAddr, pinBank   @add offset to gpioaddr
+.unreq pinBank
+
+and pinNum, #31         @pinNum %= 32
+setBit .req r3
+mov setBit, #1
+lsl setBit, pinNum      @setBit = 1<<pinNum
+.unreq pinNum
+
+teq pinVal, #0          @test equality between pinVal and 0
+.unreq pinVal
+streq setBit, [gpioAddr,#40]    @store here if pinVal == 0
+strne setBit, [gpioAddr,#28]    @if pinVal != 0 (refer controller address map)
+.unreq setBit
+.unreq gpioAddr
+pop {pc}    @return
--- source/main.s
+++ source/main.s
@@ -1,26 +1,26 @@
-/*
- * GPIO controller map
- * Base address = 0x20200000
- * 00 - 24: Function select
- * 28 - 36: Turn on pin
- * 40 - 48: Turn off pin
- * 52 - 60: Pin input
-*/
-
 .section .init
 .globl _start
 _start:
-ldr r0,=0x20200000  @GPIO controller address 
-/* This block of code enable output mode on 16th pin */
-mov r1,#1           
-lsl r1,#18          @ r1 = 2<<18 #3 bits/pin, we want the 6th pin.
-str r1,[r0,#4]      @ sets the 18th bit on 0x20200004
+b main
 
-mov r1,#1
-lsl r1,#16
+.section .text
+main:
+mov sp, #0x8000
+
+pinNum .req r0
+pinFunc .req r1
+mov pinNum, #16
+mov pinFunc, #1
+bl SetGpioFunction
+.unreq pinNum
+.unreq pinFunc
+
 
 loop$:
-str r1,[r0,#40]     @sets the turn-off bit of 16th pin
+/* Turn on LED */
+mov r0, #16 @previous SetGpioFunc could have erased r0
+mov r1, #0
+bl SetGpio
 
 /* Waiting */
 mov r2,#0x3F0000
@@ -29,7 +29,10 @@ sub r2,#1
 cmp r2,#0
 bne wait1$
 
-str r1,[r0,#28]     @sets the turn-on bit of 16th pin
+/* Turn off LED */
+mov r0, #16 @previous SetGpioFunc could have erased r0
+mov r1, #1
+bl SetGpio
 
 /* Waiting */
 mov r2,#0x3F0000
